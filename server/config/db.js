de0e8aae5054c7666a590847913362654501a2fb
vyshnavi-12 <147264006+vyshnavi-12@users.noreply.github.com>
2025-01-11 20:44:02 +0530

    Update db.js

--- server/config/db.js
+++ server/config/db.js
@@ -1,11 +1,12 @@
-const mongoose = require('mongoose');
-mongoose.set('strictQuery', false);
-const connectDB = async() => {
+const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const conn = await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
     console.log(`Database Connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(error);
+    process.exit(1);
   }
-}
-module.exports = connectDB;
+};
